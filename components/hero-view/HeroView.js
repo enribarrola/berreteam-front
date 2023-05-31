6ce060f43496b7502e25a89f3e0568c2303e9c1b
xstone <saaimonsg2@gmail.com>
2023-05-31 05:30:00 -0300

    docker

---
+++ components/hero-view/HeroView.js
@@ -0,0 +1,40 @@
+export function HeroView({data}) {
+
+    let title = null;
+    let subtitle = null;
+    let shortDescription = null;
+    let smallImage = "/img/hero.jpg";
+
+    if (data.subtitle) {
+        subtitle = data.subtitle;
+    }
+    if (data.title) {
+        title = data.title;
+    }
+    if (data.shortDescription) {
+        shortDescription = data.shortDescription;
+    }
+    if (data.smallImage) {
+        smallImage = data.smallImage;
+    }
+
+
+    return (<div className="row">
+        <div className="col-sm-4">
+            <div className="hero-inner-img  ">
+                <img src={smallImage} alt=""/>
+            </div>
+        </div>
+        <div className="col-sm-2">
+        </div>
+        <div className="col-sm-4">
+            <div className="row ">
+                <h4 className={"hero-title"}>{title}</h4>
+                <h5 className={"hero-subtitle"}>{subtitle}</h5>
+                <p className={"hero-description"}>{shortDescription}</p>
+            </div>
+        </div>
+        <div className="col-sm-2">
+        </div>
+    </div>);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-
+import {HeroView} from "../components/hero-view/HeroView";
 
 
 export default function Index(props) {
-    const crops = props;
+
+    const mdata = {
+        title: "Lorem ipsum dolor sitame",
+        subtitle:"don't harame",
+        shortDescription: "noResponseData",
+        smallImage: "/img/joystick.jpg"
+    }
 
     return (<>
-        
-        <h1>HOLA</h1>
+
+        <div className="hero-bg non-navbar-content container-fluid">
+            <div className="container">
+                <HeroView data={mdata}></HeroView>
+            </div>
+        </div>
     </>);
 }
